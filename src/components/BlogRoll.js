117aa1fadacdfd73fa7dc861f58c8dadbfb0c53d
Ryan <thecopelandcollective@gmail.com>
2023-01-03 21:13:13 -0500

    rearranging blogroll

--- src/components/BlogRoll.js
+++ src/components/BlogRoll.js
@@ -5,16 +5,16 @@ import PreviewCompatibleImage from './PreviewCompatibleImage'
 
 
 const BlogRollTemplate = (props) => {
-  
+
   const { edges: posts } = props.data.allMarkdownRemark;
 
   return (
     <div className="columns is-multiline">
       {posts &&
         posts.map(({ node: post }) => (
-          <div className="is-parent column is-6" key={post.id}>
+          <div className="is-parent column is-12" key={post.id}>
             <article
-              className={`blog-list-item tile is-child box notification ${
+              className={`blog-list-item tile is-child notification ${
                 post.frontmatter.featuredpost ? 'is-featured' : ''
               }`}
             >
