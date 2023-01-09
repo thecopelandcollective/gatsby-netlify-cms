8dfebef8085fdc3d824cfbfa595a0f229ec56e34
Ryan <thecopelandcollective@gmail.com>
2023-01-09 14:39:18 -0500

    fixing graphQL issue

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -80,14 +80,14 @@ const BlogPost = ({ data }) => {
       />
     </Layout>
     <Disqus
-        config={
-            /* Replace PAGE_URL with your post's canonical URL variable */
-            url: 'PAGE_URL',
-            /* Replace PAGE_IDENTIFIER with your page's unique identifier variable */
-            identifier: 'PAGE_IDENTIFIER',
-            /* Replace PAGE_TITLE with the title of the page */
-            title: 'PAGE_TITLE',
-        }
+      config={
+        /* Replace PAGE_URL with your post's canonical URL variable */
+        url: 'PAGE_URL',
+        /* Replace PAGE_IDENTIFIER with your page's unique identifier variable */
+        identifier: 'PAGE_IDENTIFIER',
+        /* Replace PAGE_TITLE with the title of the page */
+        title: 'PAGE_TITLE',
+      }
     />
   );
 };
