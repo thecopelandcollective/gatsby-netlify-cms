68414e564ee252f00d8ef5d7f37d4827b644be83
Ryan <thecopelandcollective@gmail.com>
2023-01-10 13:13:42 -0500

    removed Disqus

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -7,7 +7,7 @@ import Layout from "../components/Layout";
 import Content, { HTMLContent } from "../components/Content";
 import { Disqus } from 'gatsby-plugin-disqus';
 
-const Template = () => (
+// const Template = () => (
 // eslint-disable-next-line
 export const BlogPostTemplate = ({
   content,
@@ -106,14 +106,14 @@ export const pageQuery = graphql
   }
 `
 ;
-    <Disqus
-        config={
-            /* Replace PAGE_URL with your post's canonical URL variable */
-            url: 'PAGE_URL',
-            /* Replace PAGE_IDENTIFIER with your page's unique identifier variable */
-            identifier: 'PAGE_IDENTIFIER',
-            /* Replace PAGE_TITLE with the title of the page */
-            title: 'PAGE_TITLE',
-        }
-    />
-);
+//     <Disqus
+//         config={
+//             /* Replace PAGE_URL with your post's canonical URL variable */
+//             url: 'PAGE_URL',
+//             /* Replace PAGE_IDENTIFIER with your page's unique identifier variable */
+//             identifier: 'PAGE_IDENTIFIER',
+//             /* Replace PAGE_TITLE with the title of the page */
+//             title: 'PAGE_TITLE',
+//         }
+//     />
+// );
